85c586e2d454cbee46247089111fe0665dcec16e
progvar <prog.varadi@gmail.com>
2016-01-28 22:41:27 +0100

    feat(random): Get a subarray of the main text array.
    By passing a number to the random function, it will return the given number of elements in an array
    instead of just one, which is the default.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 var allText = require("./some_text.json");
 var ura = require("unique-random-array");
-
+var getRandom = ura(allText);
 module.exports = {
   all: allText,
-  random: ura(allText)
+  random: randomFunc
+}
+
+function randomFunc(number){
+  if(number === undefined){
+    return getRandom();
+  } else {
+    var texts = [];
+    for(var i = 0;i < number; i++){
+      texts.push(getRandom());
+    }
+    return texts;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,4 @@
 var allText = require('./index');
-
 var expect = require('chai').expect;
 
 describe('all', function(){
@@ -22,5 +21,13 @@ describe('random', function(){
   it('it should return a random text', function(){
     var randomItem = allText.random();
     expect(allText.all).to.include(randomItem);
-  })
+  });
+
+  it('should return the given number of texts according to the passed argument', function(){
+    var randomItems = allText.random(3);
+    expect(randomItems).to.have.length(3);
+    randomItems.forEach(function(item){
+      expect(allText.all).to.include(item);
+    });
+  });
 });
