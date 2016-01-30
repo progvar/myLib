5c89d1d54e9e5800c74f9a07a8641289b58058cc
progvar <prog.varadi@gmail.com>
2016-01-30 21:55:29 +0100

    chore(ES6 Support): Adding Babel
    Using Babel as a transpiler to support older browsers with ES5 to run greater versions of ES.

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+var allText = require('./some_text.json');
+var ura = require('unique-random-array');
+var getRandom = ura(allText);
+
+// webserver
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end(getRandom().toString());
+}).listen(port, function () {
+  console.log('Listening on ' + port);
+});
+// ^^ webserver ^^
+
+module.exports = {
+  all: allText,
+  random: randomFunc
+};
+
+function randomFunc(number) {
+  if (number === undefined) {
+    return getRandom();
+  } else {
+    var texts = [];
+    for (var i = 0; i < number; i++) {
+      texts.push(getRandom());
+    }
+    return texts;
+  }
+}
