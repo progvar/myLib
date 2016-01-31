13e4e503159daddf6421b8d6b5b9ebb587b66914
progvar <prog.varadi@gmail.com>
2016-01-31 22:24:12 +0100

    refactor(Architecture): Webserver & Module separation
    Separated the webserver and the module code as it should be, but they still form one application
    just for this test project.

--- dist/index.js
+++ dist/index.js
@@ -2,19 +2,6 @@ var allText = require('./some_text.json');
 var ura = require('unique-random-array');
 var getRandom = ura(allText);
 
-// webserver
-
-const http = require('http');
-const port = process.env.PORT || 3000;
-
-http.createServer(function (request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(getRandom().toString());
-}).listen(port, function () {
-  console.log('Listening on ' + port);
-});
-// ^^ webserver ^^
-
 module.exports = {
   all: allText,
   random: randomFunc
