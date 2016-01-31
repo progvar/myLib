8b20d2642dfe5d6c73806a94307b67a480b7fae4
progvar <prog.varadi@gmail.com>
2016-01-31 22:51:51 +0100

    fix(Wrong function name): Changed random.getRandom() to random.random().

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const random = require('./index.js');
+const random = require('./index');
 const port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end(random.getRandom().toString());
+  response.end(random.random().toString());
 }).listen(port, function(){
   console.log('Listening on ' + port);
 });
